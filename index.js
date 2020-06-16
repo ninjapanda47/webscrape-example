33957d4d7b2639a5996119eb06b55ff7a78d7781
Sandi <sandi.c.gau@gmail.com>
2020-06-15 23:06:39 -0500

    add inject jquery

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const { url, selector } = require('./custom');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
+  await page.addScriptTag({
+    url: 'https://code.jquery.com/jquery-3.2.1.min.js',
+  });
   // Get the text extraction
   const extractedText = await page.evaluate((selector) => {
     return $(selector).text().trim();
